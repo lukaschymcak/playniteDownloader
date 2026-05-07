accb821338ad2a3038b8121f58988b9988d5442e
Lukas Chymcak <lukas.chymcak@gmail.com>
2026-05-08 01:32:11 +0200

    feat: add InstalledGames to IAppHost interface

--- LuDownloader.Core/Abstractions/IAppHost.cs
+++ LuDownloader.Core/Abstractions/IAppHost.cs
@@ -4,6 +4,7 @@ namespace BlankPlugin
     {
         void OpenDownloadForAppId(string appId, string name, string imageUrl = null);
         string UserDataPath { get; }
+        InstalledGamesManager InstalledGames { get; }
         void RemoveFromHostLibrary(string playniteGameId);
         void ShowNotification(string message, bool isError = false);
     }
